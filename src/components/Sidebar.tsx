8c20b72f92a93a5808c4328a8159b6d9faa7000a
NicholasBapt <nicholasbapt@gmail.com>
2026-04-15 10:49:01 -0300

    sidebar background

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,3 +1,15 @@
 export default function Sidebar() {
-  return <div className="flex min-h-screen w-32 bg-blue-dark rounded">asd</div>;
+  return (
+    <div className="flex flex-col w-48 p-4 bg-blue-dark rounded text-white">
+      <span>Logo</span>
+      <div className="flex flex-col justify-between h-full">
+        <div className="flex flex-col">
+          <span>MENU 1</span>
+          <span>MENU 1</span>
+          <span>MENU 1</span>
+        </div>
+        <span>Recolher</span>
+      </div>
+    </div>
+  );
 }
