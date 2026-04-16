2b0af0ac49ea79ab359bb7438c5e61585df65155
NicholasBapt <nicholasbapt@gmail.com>
2026-04-15 22:12:27 -0300

    update buttons

---
+++ src/components/Button.tsx
@@ -0,0 +1,101 @@
+import React from "react";
+import Icon from "./Icon";
+import Text from "./Text";
+import { cva, type VariantProps } from "class-variance-authority";
+
+export const buttonVariants = cva(
+  `
+    flex items-center justify-center cursor-pointer
+    transition rounded-lg group gap-2
+    `,
+  {
+    variants: {
+      variant: {
+        primary: "bg-blue-base hover:bg-blue-dark",
+        sidebar: "bg-blue-dark hover:bg-blue-base",
+      },
+      size: {
+        md: "h-14 py-4 px-5",
+        sm: "h-10 px-4 p-2",
+      },
+      disabled: {
+        true: "opacity-50 pointer-events-none",
+      },
+      border: {
+        true: "border border-blue-light",
+      },
+    },
+    defaultVariants: {
+      variant: "primary",
+      size: "md",
+      disabled: false,
+      border: false,
+    },
+  },
+);
+
+export const buttonTextVariants = cva("", {
+  variants: {
+    variant: {
+      primary: "text-white",
+      sidebar: "text-white",
+    },
+    size: {
+      md: "text-[16px]",
+      sm: "text-[14px]",
+    },
+  },
+  defaultVariants: {
+    variant: "primary",
+    size: "md",
+  },
+});
+
+export const buttonIconVariants = cva("transition", {
+  variants: {
+    variant: {
+      primary: "fill-white",
+      sidebar: "fill-white",
+    },
+    size: {
+      md: "w-5 h-5",
+      sm: "w-4 h-4",
+    },
+  },
+  defaultVariants: {
+    variant: "primary",
+    size: "md",
+  },
+});
+
+interface ButtonProps
+  extends
+    Omit<React.ComponentProps<"button">, "size" | "disabled">,
+    VariantProps<typeof buttonVariants> {
+  icon?: React.ComponentProps<typeof Icon>["svg"];
+}
+
+export default function Button({
+  variant,
+  size,
+  disabled,
+  className,
+  children,
+  icon: IconComponent,
+  ...props
+}: ButtonProps) {
+  return (
+    <button
+      className={buttonVariants({ variant, size, disabled, className })}
+      {...props}
+    >
+      {IconComponent && (
+        <Icon
+          svg={IconComponent}
+          className={buttonIconVariants({ variant, size })}
+        />
+      )}
+      <Text className={buttonTextVariants({ variant, size })}>{children}</Text>
+    </button>
+  );
+}
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,12 +1,32 @@
+import Button from "./Button";
+
 export default function Sidebar() {
   return (
     <div className="flex flex-col w-48 p-4 bg-blue-dark rounded text-white">
       <span>Logo</span>
       <div className="flex flex-col justify-between h-full">
-        <div className="flex flex-col">
-          <span>MENU 1</span>
-          <span>MENU 1</span>
-          <span>MENU 1</span>
+        <div className="flex flex-col gap-2">
+          <Button
+            className="w-full justify-start"
+            size={"sm"}
+            variant={"primary"}
+          >
+            Dashboard
+          </Button>
+          <Button
+            className="w-full justify-start"
+            size={"sm"}
+            variant={"sidebar"}
+          >
+            Funcionários
+          </Button>
+          <Button
+            className="w-full justify-start"
+            size={"sm"}
+            variant={"sidebar"}
+          >
+            Contratos Ativos
+          </Button>
         </div>
         <span>Recolher</span>
       </div>
