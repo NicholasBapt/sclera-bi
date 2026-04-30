10d0b295dbf4ad8e66887adc9b3319a803f15b5e
NicholasBapt <nicholasbapt@gmail.com>
2026-04-30 16:54:06 -0300

    ajst

--- src/components/DataTableComponents/TableRow.tsx
+++ src/components/DataTableComponents/TableRow.tsx
@@ -3,7 +3,7 @@ import { cva, type VariantProps } from "class-variance-authority";
 export const tableRowVariants = cva("", {
   variants: {
     variant: {
-      primary: "odd:bg even:bg-cloudy-dark",
+      primary: "even:bg-blue-dark odd:bg",
     },
   },
   defaultVariants: {
